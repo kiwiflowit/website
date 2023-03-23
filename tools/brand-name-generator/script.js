eb7191a1bb5c913b541c6f63e2fc84d3af185aed
stefanosallemi <stefano.sallemi03@gmail.com>
2023-03-23 17:59:24 +0100

    cccccccc

--- tools/brand-name-generator/script.js
+++ tools/brand-name-generator/script.js
@@ -8,13 +8,12 @@ $('#form').on('submit', e => {
   $results.html('');
 
   items.forEach((item, idx) => {
-    var $link = $('<a class="result-item" target="_blank">').attr('href', '#').appendTo($results);
-    var $span = $('<span>').text(item).appendTo($link);
+    var $link = $('<p class="result-item" target="_blank">').attr('href', '#').appendTo($results);
+    var $span = $('<span style="transition: transform 0.2s ease-in;">').text(item).appendTo($link);
     $link.on('click', e => {
       e.preventDefault();
       var textToCopy = $span.text();
       navigator.clipboard.writeText(textToCopy);
-      alert(`Testo copiato: ${textToCopy}`);
     });
   });
 });
